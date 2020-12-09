a21090a4cf68957f2a4bbde658b766ac71390e47
boston2029 <69646049+boston2029@users.noreply.github.com>
2020-12-09 17:11:26 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace server_remote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListen = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.BackColor = System.Drawing.Color.Blue;
+            this.btnListen.FlatAppearance.BorderSize = 0;
+            this.btnListen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListen.ForeColor = System.Drawing.Color.White;
+            this.btnListen.Location = new System.Drawing.Point(91, 187);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(143, 44);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Listen On Port";
+            this.btnListen.UseVisualStyleBackColor = false;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(91, 64);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(157, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(88, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(329, 254);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnListen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace server_remote
+{
+    public partial class Form1 : Form
+    {
+        private readonly int port;
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream mainStream;
+
+        private readonly Thread Listening;
+        private readonly Thread GetImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            new Form2(int.Parse(txtPort.Text)).Show();
+            btnListen.Enabled = false;
+        }
+    }
+}
